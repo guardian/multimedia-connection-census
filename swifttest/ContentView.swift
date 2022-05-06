865d199d1b8963dd08caa963d6dfb8dfba1466f2
andy_gallagher <andy.gallagher@theguardian.com>
2022-05-06 13:59:23 +0100

    UI fiddling

--- swifttest/ContentView.swift
+++ swifttest/ContentView.swift
@@ -11,12 +11,14 @@ struct WifiStatusView :View {
     var wifiStatus:CurrentWifi
     
     var body: some View {
-        List(content: {
+        VStack(content: {
             Text(String(format:"Available networks: %d", wifiStatus.networksCount))
             Text(String(format:"Connected to: %@", wifiStatus.ssid ?? "<not connected>"))
             Text(String(format:"Channel band: %@", wifiStatus.channelBand ?? "<none>"))
             Text(String(format:"Channel number: %d", wifiStatus.channelNumber ?? -1))
             Text(String(format:"Signal-noise ratio: %02.0f%%", (wifiStatus.SNR ?? 0.0)*100))
+        }).alignmentGuide(HorizontalAlignment.leading, computeValue: { dimension in
+            dimension.width
         })
     }
 }
@@ -24,6 +26,9 @@ struct WifiStatusView :View {
 struct ContentView: View {
     @State var iconColour:Color = .yellow
     @State var wifiStatus:CurrentWifi?
+    @State var testWasRun = false
+    
+    let labelColumnWidth:CGFloat = 100.0
     
     func onClick() {
         do {
@@ -39,17 +44,25 @@ struct ContentView: View {
         VStack(content: {
             List(content: {
                 HStack(alignment: .top, spacing: 10, content: {
-                    Text("WiFi")
                     Circle().size(
-                        CGSize(width: 18, height: 18)).foregroundColor(iconColour)
+                        CGSize(width: 18, height: 18))
+                        .foregroundColor(iconColour)
+                        .frame(width:20, height:20)
+                    Text("WiFi").multilineTextAlignment(.leading)
+                        .frame(width:labelColumnWidth, height:20)
                     if let activeWifi=wifiStatus {
-                        WifiStatusView(wifiStatus: activeWifi).frame(width:200, height: 200)
+                        WifiStatusView(wifiStatus: activeWifi)
                     } else {
-                        Text("No wifi")
+                        Text(testWasRun ? "No wifi" : "Run test to see results")
                     }
                 }).padding(.all, 20)
                 HStack(alignment: .center, spacing: 10, content: {
-                    Text("Placeholder")
+                    Circle().size(
+                        CGSize(width: 18, height: 18))
+                        .foregroundColor(iconColour)
+                        .frame(width:20, height:20)
+                    Text("EU Cloud")
+                        .multilineTextAlignment(.leading).frame(width:labelColumnWidth, height:20)
                 }).padding(.all, 20)
                 HStack(alignment: .bottom, spacing: 10, content: {
                     Text("Placeholder")
